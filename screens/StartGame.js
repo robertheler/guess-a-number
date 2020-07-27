2c8903bd32404b716622c400b54318e3fe8ff55f
Robert Heler <robertheler@gmail.com>
2020-07-27 14:40:59 -0700

    Added confirmation card and input validation

--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,39 +1,90 @@
-import React, {useState} from "react";
-import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  Button,
+  Keyboard,
+  Alert
+} from "react-native";
 import Card from "../components/Card";
 import Colors from "../theme/colors";
 import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
 
 export default StartGame = props => {
   const [enteredValue, setEnteredValue] = useState("");
-  const inputHandler = (inputText) => {
-    setEnteredValue(inputText.replace(/[^0-9]/g, ""))
-  }
+  const [hasConfirmed, setHasConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+
+  const inputHandler = inputText => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
+
+  const resetInputHandler = () => {
+    setEnteredValue("");
+    setHasConfirmed(false);
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert("Invalid Number!", "Number has to be between 1 and 99", [
+        { text: "Okay", style: "destructive", onPress: resetInputHandler }
+      ]);
+      return;
+    }
+    setHasConfirmed(true);
+    setSelectedNumber(chosenNumber);
+    setEnteredValue("");
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game!</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Enter a number</Text>
-        <Input
-          style={styles.input}
-          blurOnSubmit
-          autoCapitalize="none"
-          autoCorrect={false}
-          keyboardType="number-pad"
-          maxLength={2}
-          onChangeText={inputHandler}
-          value={enteredValue}
-        />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button title="Reset" onPress={() => {}} color={Colors.primary} />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Enter a number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="number-pad"
+            maxLength={2}
+            onChangeText={inputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title="Reset"
+                onPress={resetInputHandler}
+                color={Colors.primary}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Confirm"
+                onPress={confirmInputHandler}
+                color={Colors.accent}
+              />
+            </View>
           </View>
-          <View style={styles.button}>
-            <Button title="Confirm" onPress={() => {}} color={Colors.accent} />
-          </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+        {hasConfirmed ? (
+          <Card style={styles.summaryContainer}>
+            <Text style={{ textAlign: "center" }}>You selected:</Text>
+            <NumberContainer>{selectedNumber}</NumberContainer>
+            <Button title="START GAME" />
+          </Card>
+        ) : (
+          undefined
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -67,5 +118,9 @@ const styles = StyleSheet.create({
   input: {
     width: 80,
     textAlign: "center"
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: "center"
   }
 });
